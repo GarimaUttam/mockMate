567f254a40985cec766ade99704f7be1c0aa7add
GarimaUttam <garimauttam191203@gmail.com>
2024-10-07 12:27:47 +0530

    added shadcn components

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>home page</div>
+    <div>home page
+    <Button>Get started</Button>
+    </div>
   );
 }
